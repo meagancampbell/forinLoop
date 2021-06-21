b92ebcd42e007226ac796561bdcc5b6e78476f64
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-21 21:16:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let tortoiseOne = {
+    species: "Galapagos Tortoise",
+    name: "Pete",
+    weight: 919,
+    age: 85,
+    diet: ["pumpkins", "lettuce", "cabbage"]
+};
+
+for (item in tortoiseOne) {
+   console.log(item + ", " + tortoiseOne[item]);
+}
+// Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
